7a2c709b68f3238b76e3e2e82440fead0a56b3b5
josecalderon2 <115597425+josecalderon2@users.noreply.github.com>
2024-05-19 21:53:32 -0600

    LISTA
    buscar lista por usuario y mostrar los datos de lista

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import lista from "./rutas/lista.js";
 
 const app = express();
 
@@ -7,6 +8,8 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
+
+app.use("/api", lista);
 
 app.listen(PORT, () => {
   console.log(`App lista y perfecta corriendo en el puerto ${PORT}`);
---
+++ rutas/lista.js
@@ -0,0 +1,35 @@
+import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const router = Router();
+const prisma = new PrismaClient();
+
+router.get("/obtenerlista", async (req, res) => {
+    try {
+      const detallelista = await prisma.listas.findMany();
+      res.status(200).json(detallelista);
+    } catch (error) {
+      console.error("Error al obtener los detalles de la lista:", error);
+      res.status(500).json({ error: "Error interno del servidor" });
+    }
+  });
+
+  router.get("/obtenerlista/:idUsuario", async (req, res) => {
+    const { idUsuario } = req.params;
+    
+    try {
+        const detalleLista = await prisma.listas.findMany({
+            where: {
+                id_usuario: parseInt(idUsuario)
+            }
+        });
+        
+        res.status(200).json(detalleLista);
+    } catch (error) {
+        console.error("Error al obtener los detalles de la lista:", error);
+        res.status(500).json({ error: "Error interno del servidor" });
+    }
+});
+
+  export default router;
+  
