9b43c0805e8eecfc1ae7964b61d984962e89024b
xaldakr <50474978+xaldakr@users.noreply.github.com>
2024-05-20 00:09:07 -0600

    minor fixes

--- rutas/favoritos.js
+++ rutas/favoritos.js
@@ -5,75 +5,78 @@ const router = Router();
 const prisma = new PrismaClient();
 
 router.get("/obtenerfav", async (req, res) => {
-    try {
-      const detallefav = await prisma.favoritos.findMany();
-      res.status(200).json(detallefav);
-    } catch (error) {
-      console.error("Error al obtener los detalles de favoritos:", error);
-      res.status(500).json({ error: "Error interno del servidor" });
+  try {
+    const detallefav = await prisma.favoritos.findMany();
+    res.status(200).json(detallefav);
+  } catch (error) {
+    console.error("Error al obtener los detalles de favoritos:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+});
+router.post("/addfav", async (req, res) => {
+  const { idUsuario, idReceta } = req.body;
+
+  try {
+    const favoritoExistente = await prisma.favoritos.findFirst({
+      where: {
+        id_usuario: parseInt(idUsuario),
+        id_receta: parseInt(idReceta),
+      },
+    });
+
+    if (favoritoExistente) {
+      await prisma.favoritos.delete({
+        where: {
+          id_favorito: parseInt(favoritoExistente.id_favorito),
+        },
+      });
+      return res.status(202).json({ mensaje: "Receta eliminada de favoritos" });
     }
-  });
-  router.post("/addfav", async (req, res) => {
-    const { idUsuario, idReceta } = req.body;
 
-    try {
-        const favoritoExistente = await prisma.favoritos.findFirst({
-            where: {
-                id_usuario: parseInt(idUsuario),
-                id_receta: parseInt(idReceta)
-            }
-        });
+    const ultimoOrden = await prisma.favoritos.findFirst({
+      where: {
+        id_usuario: parseInt(idUsuario),
+      },
+      orderBy: {
+        orden: "desc",
+      },
+      select: {
+        orden: true,
+      },
+    });
 
-        if (favoritoExistente) {
-            return res.status(202).json({ mensaje: "La receta ya está en favoritos" });
-        }
+    const nuevoOrden = ultimoOrden ? ultimoOrden.orden + 1 : 1;
 
-    
-        const ultimoOrden = await prisma.favoritos.findFirst({
-            where: {
-                id_usuario: parseInt(idUsuario)
-            },
-            orderBy: {
-                orden: 'desc'
-            },
-            select: {
-                orden: true
-            }
-        });
+    await prisma.favoritos.create({
+      data: {
+        id_usuario: parseInt(idUsuario),
+        id_receta: parseInt(idReceta),
+        orden: nuevoOrden,
+      },
+    });
 
-        const nuevoOrden = ultimoOrden ? ultimoOrden.orden + 1 : 1;
-
-        await prisma.favoritos.create({
-            data: {
-                id_usuario: parseInt(idUsuario),
-                id_receta: parseInt(idReceta),
-                orden: nuevoOrden  
-            }
-        });
-
-        res.status(201).json({ mensaje: "Receta agregada a favoritos" });
-    } catch (error) {
-        console.error("Error al agregar receta a favoritos:", error);
-        res.status(500).json({ error: "Error interno del servidor" });
-    }
+    res.status(201).json({ mensaje: "Receta agregada a favoritos" });
+  } catch (error) {
+    console.error("Error al agregar receta a favoritos:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
 });
 
 router.delete("/eliminarfav/:idf", async (req, res) => {
-    const { idf } = req.params;
+  const { idf } = req.params;
 
-    try {
-        await prisma.favoritos.delete({
-            where: {
-                id_favorito: parseInt(idf)
-            }
-        });
+  try {
+    await prisma.favoritos.delete({
+      where: {
+        id_favorito: parseInt(idf),
+      },
+    });
 
-        res.status(200).json({ mensaje: "Receta eliminada de favoritos" });
-    } catch (error) {
-        console.error("Error al eliminar receta de favoritos:", error);
-        res.status(500).json({ error: "Error interno del servidor" });
-    }
+    res.status(200).json({ mensaje: "Receta eliminada de favoritos" });
+  } catch (error) {
+    console.error("Error al eliminar receta de favoritos:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
 });
-
 
 export default router;
--- rutas/lista.js
+++ rutas/lista.js
@@ -5,31 +5,30 @@ const router = Router();
 const prisma = new PrismaClient();
 
 router.get("/obtenerlista", async (req, res) => {
-    try {
-      const detallelista = await prisma.listas.findMany();
-      res.status(200).json(detallelista);
-    } catch (error) {
-      console.error("Error al obtener los detalles de la lista:", error);
-      res.status(500).json({ error: "Error interno del servidor" });
-    }
-  });
-
-  router.get("/obtenerlista/:idUsuario", async (req, res) => {
-    const { idUsuario } = req.params;
-    
-    try {
-        const detalleLista = await prisma.listas.findMany({
-            where: {
-                id_usuario: parseInt(idUsuario)
-            }
-        });
-        
-        res.status(200).json(detalleLista);
-    } catch (error) {
-        console.error("Error al obtener los detalles de la lista:", error);
-        res.status(500).json({ error: "Error interno del servidor" });
-    }
+  try {
+    const detallelista = await prisma.listas.findMany();
+    res.status(200).json(detallelista);
+  } catch (error) {
+    console.error("Error al obtener los detalles de la lista:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
 });
 
-  export default router;
-  
+router.get("/obtenerlista/:idUsuario", async (req, res) => {
+  const { idUsuario } = req.params;
+
+  try {
+    const detalleLista = await prisma.listas.findMany({
+      where: {
+        id_usuario: parseInt(idUsuario),
+      },
+    });
+
+    res.status(200).json(detalleLista);
+  } catch (error) {
+    console.error("Error al obtener los detalles de la lista:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+});
+
+export default router;
