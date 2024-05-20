90ab38ca27f9688142aeeefabf626b1cfa4e34b4
xaldakr <50474978+xaldakr@users.noreply.github.com>
2024-05-19 23:28:34 -0600

    Chistoso

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import lista from "./rutas/lista.js";
 import favoritos from "./rutas/favoritos.js";
+import login from "./rutas/login.js";
 
 const app = express();
 
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use("/api", lista);
 app.use("/api", favoritos);
+app.use("/api", login);
 
 app.listen(PORT, () => {
   console.log(`App lista y perfecta corriendo en el puerto ${PORT}`);
