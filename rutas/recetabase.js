cf430bf1ea48e40fa1c3adf6fb9a47db7f75b9ae
Kevin Daniel Martínez <140560743+RodriguezKevin8@users.noreply.github.com>
2024-05-20 11:49:54 -0600

    se agrego el endpoint que recibe id y muestra los favoritos

--- rutas/recetabase.js
+++ rutas/recetabase.js
@@ -57,4 +57,53 @@ router.get("/receta", async (req, res) => {
     res.status(500).json({ error: "Error interno del servidor" });
   }
 });
+
+router.get("/receta/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    let detalles;
+
+    if (id) {
+      detalles = await prisma.favoritos.findMany({
+        where: {
+          id_usuario: parseInt(id),
+        },
+        include: {
+          Recetas: {
+            include: {
+              Ingredientes: true,
+              Usuarios: { select: { nombre: true } },
+              Resena: { select: { valor: true } },
+            },
+          },
+        },
+        take: 30,
+      });
+
+      detalles = detalles.map((fav) => fav.Recetas);
+    }
+
+    detalles.forEach((receta) => {
+      if (receta.Resena.length > 0) {
+        const totalResenas = receta.Resena.length;
+        const sumResenas = receta.Resena.reduce(
+          (sum, resena) => sum + resena.valor,
+          0
+        );
+        receta.promedioResenas = sumResenas / totalResenas;
+        receta.cantidadResenas = totalResenas;
+      } else {
+        receta.promedioResenas = 0;
+        receta.cantidadResenas = 0;
+      }
+    });
+
+    res.status(200).json(detalles);
+  } catch (error) {
+    console.error("Error al obtener los detalles de la receta:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+});
+
 export default router;
